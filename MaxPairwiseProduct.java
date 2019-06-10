77f525ca552a721ac6238b956b74e31322a30716
Vladislav Kostylev <kostylev.v@gmail.com>
2019-06-10 21:17:23 +0300

    Initial commit

---
+++ MaxPairwiseProduct.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class MaxPairwiseProduct {
+    static long getMaxPairwiseProduct(int[] numbers) {
+        int max_product = 0;
+        int maxIndexOne = 0;
+        int maxOne = 0;
+        int maxTwo = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+          if (numbers[i] > maxOne) {
+            maxOne = numbers[i];
+            maxIndexOne = i;
+          }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+          if (i != maxIndexOne && numbers[i] > maxTwo) {
+            maxTwo = numbers[i];
+          }
+        }
+
+        return (long)maxOne*maxTwo;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                    InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
